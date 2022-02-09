0179c7c26c7f653122bae61c8e1d3069a383584b
ROBERT1830 <robert@beatingbrain.com>
2022-02-09 13:03:24 +0100

    Calculator Screen events and states

--- app/src/main/java/com/robertconstantindinescu/imc_cleanarchitecture/feature_calculate_imc/presentation/calculator_screen/CalculatorState.kt
+++ app/src/main/java/com/robertconstantindinescu/imc_cleanarchitecture/feature_calculate_imc/presentation/calculator_screen/CalculatorState.kt
@@ -1,3 +1,13 @@
 package com.robertconstantindinescu.imc_cleanarchitecture.feature_calculate_imc.presentation.calculator_screen
 
-sealed class CalculatorState
+import com.robertconstantindinescu.imc_cleanarchitecture.feature_calculate_imc.domain.util.Gender
+
+/**
+ * Here we are defining a state object
+ */
+data class CalculatorState(
+    val fieldText:String,
+    val hintText:String,
+    val gender: Gender = Gender.Female,
+    val isHintVisible: Boolean? = false
+)
