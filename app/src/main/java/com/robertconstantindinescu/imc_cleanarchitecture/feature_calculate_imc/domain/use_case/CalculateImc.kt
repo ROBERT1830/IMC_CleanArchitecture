f90f8b818755936eedb70a29dc77c8577c058bee
ROBERT1830 <robert@beatingbrain.com>
2022-02-09 13:41:23 +0100

    CalculateImc.kt use case and GetSingleImcRecord.kt use case

---
+++ app/src/main/java/com/robertconstantindinescu/imc_cleanarchitecture/feature_calculate_imc/domain/use_case/CalculateImc.kt
@@ -0,0 +1,19 @@
+package com.robertconstantindinescu.imc_cleanarchitecture.feature_calculate_imc.domain.use_case
+
+import kotlin.math.pow
+
+/**
+ * This use case doesn't need the repo becuse will not acces database
+ */
+class CalculateImc {
+
+
+    operator fun invoke(weight: Double, height: Double): Double {
+
+        //first calculate the result
+        return height / weight.pow(2.0)
+
+
+    }
+
+}
--- app/src/main/java/com/robertconstantindinescu/imc_cleanarchitecture/feature_calculate_imc/presentation/calculator_screen/CalculatorEvent.kt
+++ app/src/main/java/com/robertconstantindinescu/imc_cleanarchitecture/feature_calculate_imc/presentation/calculator_screen/CalculatorEvent.kt
@@ -1,12 +1,14 @@
 package com.robertconstantindinescu.imc_cleanarchitecture.feature_calculate_imc.presentation.calculator_screen
 
 import androidx.compose.ui.focus.FocusState
+import com.robertconstantindinescu.imc_cleanarchitecture.feature_calculate_imc.domain.util.Gender
 
 sealed class CalculatorEvent{
     data class EnterName(val value:String): CalculatorEvent()
     data class EnterWeight(val value:String): CalculatorEvent()
     data class EnterHeight(val value:String): CalculatorEvent()
-    data class GetResult(val value:String): CalculatorEvent()
+    //data class GetResult(val value:String): CalculatorEvent()  --> I don't think that this could be an event because the user doesn't interact with that field
+    data class CalculateImc(val weight:Double, val height:Double):CalculatorEvent()
 
     data class ChangeNameFocus(val focusState: FocusState): CalculatorEvent()
     data class ChangeWeightFocus(val focusState: FocusState): CalculatorEvent()
